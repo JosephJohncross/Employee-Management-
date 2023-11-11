a80f4dd362ce75b081a3c324b2040030386181db
JosephJohncross <josephibochi@gmail.com>
2023-11-11 15:08:17 +0100

    feature: added services for MediatR and NetArchTest

---
+++ src/Test/EmployeeManagement.ArchitectureTest/ControllerTest/ControllerTest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetArchTest.Rules;
+
+namespace EmployeeManagement.ArchitectureTest.ControllerTest
+{
+    public class ControllerTest
+    {
+
+    }
+}
